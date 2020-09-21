3939046ff9b03240067b6df440bdbaff7ca97ea5
Unknowncall <zharvey3131@gmail.com>
2020-09-21 17:01:53 -0500

    Added inversion counting skele code

---
+++ src/me/compsci/theory/InversionCounting.java
@@ -0,0 +1,30 @@
+package me.compsci.theory;
+
+public class InversionCounting {
+
+    private final int mergedArray[];
+    private final int array[];
+    private final int n;
+
+    public InversionCounting(int[] array, int n) {
+        this.array = array;
+        this.mergedArray = new int[n];
+        this.n = n;
+    }
+
+    public int bruteForce() {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (array[i] > array[j])
+                    count++;
+        return count;
+    }
+
+    public int countInversions() {
+        return countInversions(0, n - 1);
+    }
+
+    private int countInversions(int left, int right) { // complete this function
+    }
+}
