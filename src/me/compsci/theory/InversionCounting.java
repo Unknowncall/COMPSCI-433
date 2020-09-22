c9c30430234e36b1f2c0d7bb3aa2a8fb8500dca7
Ryan Beatty <rbeatty1196@gmail.com>
2020-09-22 17:52:27 -0500

    InversionCounting finished

--- src/me/compsci/theory/InversionCounting.java
+++ src/me/compsci/theory/InversionCounting.java
@@ -1,67 +1,59 @@
 package me.compsci.theory;
 
-import java.util.Arrays;
+import java.util.Arrays; 
 
 public class InversionCounting {
 
-    private final int mergedArray[];
-    private final int array[];
-    private final int n;
+	//private final int mergedArray[];
+	private final int array[];
+	private final int n;
 
-    public InversionCounting(int[] array, int n) {
-        this.array = array;
-        this.mergedArray = new int[n];
-        this.n = n;
-    }
+	public InversionCounting(int[] array, int n) {
+		this.array = array;
+		//this.mergedArray = new int[n];
+		this.n = n;
+	}
 
-    public int bruteForce() {
-        int count = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++)
-                if (array[i] > array[j])
-                    count++;
-        return count;
-    }
+	public int bruteForce() {
+		int count = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j < n; j++)
+				if (array[i] > array[j])
+					count++;
+		return count;
+	}
 
-    public int countInversions() {
-        return countInversions(0, n - 1);
-    }
+	public int countInversions() {
+		return countInversions(0, n - 1);
+	}
+	
+	private int countInversions(int left, int right) {  
+		int count = 0; 
 
-    /*
-        a[i] > a[j]; where i < j
-        If an element is at a lower index and is larger then something at a higher index\
-     */
-    private int countInversions(int left, int right) { // complete this function
-        if (right - left <= 2) {
-            if (array[left] > array[right] && left < right) {
-                return 1;
-            }
-            return 0;
-        }
-        int mid = (left + right) / 2;
-        int count = 0;
-        count += countInversions(left, mid);
-        count += countInversions(mid + 1, right);
-        int i = left, j = mid + 1, k = left;
-        while (i <= mid && j <= right) {
-            if (array[j] < array[i]) {
-                mergedArray[k++] = array[j++];
-            } else {
-                count += mid - i + 1;
-                mergedArray[k++] = array[i++];
-            }
-        }
-        while (i <= mid) {
-            mergedArray[k++] = array[i++];
-        }
-        while (j <= right) {
-            mergedArray[k++] = array[j++];
-        }
-        i = left;
-        while (i <= right) {
-            array[i] = mergedArray[i];
-            i++;
-        }
-        return count;
-    }
+		if (left < right) { 
+			int mid = (left + right) / 2; 
+			count += countInversions(left, mid); 
+			count += countInversions(mid + 1, right);
+			
+			int[] leftArr = Arrays.copyOfRange(array, left, mid + 1); 
+			int[] rightArr = Arrays.copyOfRange(array, mid + 1, right + 1); 
+			int i = 0, j = 0, k = left; 
+			while (i < leftArr.length && j < rightArr.length) { 
+				if (leftArr[i] <= rightArr[j]) 
+					array[k++] = leftArr[i++]; 
+				else { 
+					array[k++] = rightArr[j++]; 
+					count += (mid + 1) - (left + i); 
+				} 
+			} 
+			while (i < leftArr.length) { 
+				array[k++] = leftArr[i++];
+			}
+			while (j < rightArr.length) {
+				array[k++] = rightArr[j++];
+			}
+		} 
+
+		return count;
+	}
 }
